fb593ececcf8973ff2868b0aae9f1734fc15188c
Ben 'Sticky' Nelson <ben@groovitation.com>
2026-03-10 16:07:05 -0600

    Fix Android external browser return script escaping

--- app/src/main/java/io/blaha/groovitation/ExternalBrowserReturnEvent.kt
+++ app/src/main/java/io/blaha/groovitation/ExternalBrowserReturnEvent.kt
@@ -1,12 +1,36 @@
 package io.blaha.groovitation
 
-import org.json.JSONObject
-
 object ExternalBrowserReturnEvent {
     const val EVENT_NAME = "groovitation:external-browser-return"
 
     fun buildScript(url: String): String {
-        val safeUrl = JSONObject.quote(url)
+        val safeUrl = buildJsStringLiteral(url)
         return "window.dispatchEvent(new CustomEvent('$EVENT_NAME', { detail: { url: $safeUrl } }));"
+    }
+
+    private fun buildJsStringLiteral(value: String): String {
+        val escaped = buildString(value.length + 2) {
+            append('"')
+            value.forEach { ch ->
+                when (ch) {
+                    '\\' -> append("\\\\")
+                    '"' -> append("\\\"")
+                    '\b' -> append("\\b")
+                    '\u000C' -> append("\\f")
+                    '\n' -> append("\\n")
+                    '\r' -> append("\\r")
+                    '\t' -> append("\\t")
+                    else -> {
+                        if (ch.code < 0x20) {
+                            append("\\u%04x".format(ch.code))
+                        } else {
+                            append(ch)
+                        }
+                    }
+                }
+            }
+            append('"')
+        }
+        return escaped
     }
 }
