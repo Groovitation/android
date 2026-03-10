c0c3b7140babc6bdc777390a2b1a45861e0e08a0
Ben 'Sticky' Nelson <ben@groovitation.com>
2026-03-10 15:28:30 -0600

    Bump Android version for issue 274 recovery

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "io.blaha.groovitation"
         minSdk = 28
         targetSdk = 35
-        versionCode = 69
-        versionName = "1.0.68"
+        versionCode = 70
+        versionName = "1.0.69"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
