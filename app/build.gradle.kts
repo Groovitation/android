fb79a43bf9c0a04ffb290e80fe6251a8657d9d8c
Ben 'Sticky' Nelson <ben@groovitation.com>
2026-03-10 12:18:16 -0600

    Bump Android version for issue 26 Pelias r5

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "io.blaha.groovitation"
         minSdk = 28
         targetSdk = 35
-        versionCode = 61
-        versionName = "1.0.60"
+        versionCode = 62
+        versionName = "1.0.61"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
