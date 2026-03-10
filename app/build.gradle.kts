b3d45e415cdaf5b53a1a418e41a96019ea822ce1
Ben 'Sticky' Nelson <ben@groovitation.com>
2026-03-10 15:43:29 -0600

    Bump Android version for issue 158 avatar r7

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "io.blaha.groovitation"
         minSdk = 28
         targetSdk = 35
-        versionCode = 70
-        versionName = "1.0.69"
+        versionCode = 71
+        versionName = "1.0.70"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
