d1c8f41125a5104ab4a1019c8c45ea8b7cadc16d
Ben 'Sticky' Nelson <ben@groovitation.com>
2026-04-02 13:40:05 -0600

    build: bump android version to 1.0.111

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -24,8 +24,8 @@ android {
         applicationId = "io.blaha.groovitation"
         minSdk = 28
         targetSdk = 35
-        versionCode = 111
-        versionName = "1.0.110"
+        versionCode = 112
+        versionName = "1.0.111"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         testInstrumentationRunnerArguments["clearPackageData"] = "true"
